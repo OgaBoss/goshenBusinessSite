0cd6987fdc0c57d3ad6087752b9fad7a409355f7
Adebayo Oluwadamilola <adebayooluwadamilola@MacBook-Pro-3.local>
2017-03-16 22:17:53 +0100

    Added bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,7 +84,7 @@
                 </div>
 
                 <div class="links">
-                    <a target="_blank" href="https://deals4meals.com/authorize/user_login?response_type=code&client_id=a518d648aa2fb1ad&redirect_uri=https%3A%2F%2Fgoshensoftinc.herokuapp.com%2Foauth2%2Fcallback">Login With Campus Credit</a>
+                    <a  class="btn btn-primary" target="_blank" href="https://deals4meals.com/authorize/user_login?response_type=code&client_id=a518d648aa2fb1ad&redirect_uri=https%3A%2F%2Fgoshensoftinc.herokuapp.com%2Foauth2%2Fcallback">Login With Campus Credit</a>
                 </div>
             </div>
         </div>
