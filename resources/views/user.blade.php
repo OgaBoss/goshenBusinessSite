86c0259ad6642b088f02092a5ced6df63e1d17ac
Adebayo Oluwadamilola <adebayooluwadamilola@MacBook-Pro-3.local>
2017-03-16 22:14:37 +0100

    Added bootstrap

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Laravel</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -76,7 +77,7 @@
         </div>
         @if(!isset($user))
             <div class="links">
-                <a target="_blank" href="https://deals4meals.com/authorize/user_login?response_type=code&client_id=a518d648aa2fb1ad&redirect_uri=https%3A%2F%2Fgoshensoftinc.herokuapp.com%2Foauth2%2Fcallback">Login With Campus Credit</a>
+                <a class="btn btn-primary" target="_blank" href="https://deals4meals.com/authorize/user_login?response_type=code&client_id=a518d648aa2fb1ad&redirect_uri=https%3A%2F%2Fgoshensoftinc.herokuapp.com%2Foauth2%2Fcallback">Login With Campus Credit</a>
             </div>
         @else
             <div class="links">
